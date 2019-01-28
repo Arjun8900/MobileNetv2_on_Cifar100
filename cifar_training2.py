8ce3f1f507863609e9e56cf45ec54b08c54ff2c6
Arjun8900 <kanwal.arjun8900@gmail.com>
2019-01-28 17:04:04 +0530

    Update cifar_training2.py

--- cifar_training2.py
+++ cifar_training2.py
@@ -36,10 +36,10 @@ x_train,y_train = unison_shuffled_copies(x_train,y_train)
 
 
 
-count1 = x_train.shape[0]
-count2 = x_test.shape[0]
+#count1 = x_train.shape[0]
+#count2 = x_test.shape[0]
 
-print(count1, ' ' ,count2)
+#print(count1, ' ' ,count2)
 print(x_train.shape)
 
 base_model = MobileNetV2(weights='imagenet', include_top=False)
@@ -65,61 +65,3 @@ model.save('mv2_cifar100_2.model')
 
 
 
-
-
-'''
-
-datagen1 = ImageDataGenerator(
-        rescale=1. / 255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        rotation_range=90,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        horizontal_flip=True)
-datagen2 = ImageDataGenerator(rescale=1. / 255)
-datagen1.fit(x_train)
-datagen2.fit(x_test)
-
-train_generator = datagen1.flow(x_train, y_train, 
-        shuffle = True,
-        #target_size=(size, size),
-        batch_size=batch,
-        #class_mode='categorical')
-        )
-test_generator = datagen2.flow(x_test, y_test, 
-        #shuffle = True,
-        #target_size=(size, size),
-        batch_size=batch,
-        #class_mode='categorical')
-        )
-# Fine Tuning 
-
-base_model = MobileNetV2(weights='imagenet', include_top=False)
-#base_model = MobileNetV2(weights='imagenet')
-
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-
-x = Reshape((1, 1, 1280))(x)
-x = Dropout(0.3, name='Dropout')(x)
-x = Conv2D(num_classes, (1, 1), padding='same')(x)     # num Classes defined
-
-x = Activation('softmax', name='softmax')(x)
-output = Reshape((num_classes,))(x)   # Reshaping to 100 classes
-#x=Dense(128,activation='relu')(x)
-#preds=Dense(2,activation='softmax')(x) 
-model=Model(inputs = base_model.input, outputs = output)
-#plot_model(model, to_file = 'MobileNetv2.png', show_shapes = True)
-
-for layer in model.layers:
-    layer.trainable = True
-
-model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-#hist = model.fit(x_train,y_train_new_cat, epochs = 30, validation_split = 0.1, shuffle = True, batch_size = 128)
-hist = model.fit_generator(train_generator, steps_per_epoch=count1//batch, validation_steps = count2//batch, epochs = 3)
-model.save('mv2_cifar100.model')
-
-
-'''
